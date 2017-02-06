99eb8eb35541f6b784c0ca12669167c1116c1166
juancgg <juancgg@hotmail.es>
2017-02-06 23:20:26 +0100

    ok

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -8,20 +8,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.Language;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslationResult;
+
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet("/SimpleServlet")
+@WebServlet(“/SimpleServlet”)
 public class SimpleServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().print("Hello World!");
-    }
+/**
+ * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+ */
+     @Override
+	 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 response.setContentType(“text/html”);
+	     response.getWriter().print(translateToSpanish(“This is a test”));
+	 }
 
+	 private String translateToSpanish(String text){
+		LanguageTranslator service = new LanguageTranslator();
+		service.setUsernameAndPassword(“87a31e67-e51f-4d72-b00c-a490d92f441f”, “sh2jisW181Od”);
+		TranslationResult translationResult = service.translate(text, Language.ENGLISH, Language.SPANISH).execute();
+		return translationResult.getFirstTranslation();
+	 }
 }
